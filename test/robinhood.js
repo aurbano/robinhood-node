310b918cc6d4b42ce6bdb02d0cab041ea741c95e
Alejandro U. Alvarez <aluralma@gmail.com>
2015-05-06 10:04:14 +0100

    Update robinhood.js

--- test/robinhood.js
+++ test/robinhood.js
@@ -2,7 +2,6 @@
  * Robinhood API NodeJS Wrapper - Tests
  * @author Alejandro U. Alvarez
  * @license AGPLv3 - See LICENSE file for more details
- * @version 0.0.1
  */
 
 var should = require('should'),
