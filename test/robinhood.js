041ad0b7fa42d7cd6ebef10951927da7aa394b4f
Alejandro U. Alvarez <aluralma@gmail.com>
2015-05-06 10:04:05 +0100

    Update robinhood.js

--- test/robinhood.js
+++ test/robinhood.js
@@ -5,4 +5,28 @@
  * @version 0.0.1
  */
 
-// Not ready yet
+var should = require('should'),
+  Robinhood = require('../src/robinhood.js');
+  
+describe('Robinhood API', function() {
+  var trader = null;
+  
+  before(function(done){
+    // I don't have a valid username at the moment
+    // so can't really test this
+    trader = Robinhood({
+      username: 'user',
+      password: 'pass'
+    });
+  });
+  
+  it('Should get Quote data', function(done) {
+    trader.quote_data('GOOG', function(err, httpResponse, body){
+      if(err){
+        done(err);
+      }
+      console.log('Quote data:', body);
+      done();
+    });
+  });
+});
