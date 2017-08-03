671db618b6b25080165ddb6f80a0fdeb0fbc8f63
Matthew Herron <matthew.herron77@gmail.com>
2017-08-03 11:16:52 -0500

    Updated request headers to use latest iOS/app version user agent and latest RH api release (#52)

--- src/robinhood.js
+++ src/robinhood.js
@@ -76,9 +76,9 @@ function RobinhoodWebApi(opts, callback) {
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5',
         'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-        'X-Robinhood-API-Version': '1.0.0',
         'Connection': 'keep-alive',
-        'User-Agent': 'Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)'
+        'X-Robinhood-API-Version': '1.152.0',
+        'User-Agent': 'Robinhood/5.32.0 (com.robinhood.release.Robinhood; build:3814; iOS 10.3.3)'
     };
     _setHeaders();
     if (!_private.auth_token) {
